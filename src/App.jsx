f16071e9c3f9234f879410db69ddac604954b9b7
allan_hanauer <allan.s.hanauer@gmail.com>
2023-11-18 07:52:46 -0300

    INITIALIZED PROJECT

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Day DD Month MM Year YYYY -- years -- months -- days
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
